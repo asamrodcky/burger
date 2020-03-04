8770b5fa68127b5ddc504557b2bf00159e7fd2ee
samrod826 <samrodriguez826@gmail.com>
2020-03-04 00:16:42 -0500

    heroku db setup

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,14 @@ var connection;
 
 // For Heroku Deployment vs. Local MySQL Database
 
-connection = mysql.createConnection(process.env.JAWSDB_URL || {
+CLEARDB_URL = {
+  host     : 'us-cdbr-iron-east-04.cleardb.net',
+  user     : 'b33f62c1def585',
+  password : '2d5d0836',
+  database : 'heroku_f948ce4dd39346b'
+}
+
+connection = mysql.createConnection(process.env.CLEARDB_URL || {
   host     : 'localhost',
   user     : 'root',
   password : 'password',
